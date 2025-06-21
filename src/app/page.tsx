22a6d96845f9ffbf5cf36cbacada6c5614d64b8b
Ryan Eka <ryanzzeka@gmail.com>
2025-06-21 23:40:52 +0700

    penambahan halaman login & register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import AuthPage from "../pages/AuthPage";
+
 export default function Home() {
-  return <div className="min-h-screen w-full flex items-center justify-center">Budget Tracker Deacourse</div>;
+  return <AuthPage/>
 }
